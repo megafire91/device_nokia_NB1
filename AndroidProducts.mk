521e59322054c494ed25416317d733a5bdd6dbbe
Marcel <marcel6260@gmail.com>
2022-07-22 08:39:24 +0200

    commited changes for cherish

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_NB1.mk
+    $(LOCAL_DIR)/cherish_NB1.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_NB1-user \
-    aosp_NB1-userdebug \
-    aosp_NB1-eng
+    cherish_NB1-user \
+    cherish_NB1-userdebug \
+    cherish_NB1-eng
