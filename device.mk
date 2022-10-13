935bb9f0acdd98a723bfd0a972513477c059913c
megafire91 <marcel6260@gmail.com>
2022-10-13 23:42:50 +0200

    changed to soundtrigger 2.1 to fix bootloop

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.service \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_PACKAGES += \
     audio.r_submix.default \
